6093e55296b3d047a9d69a98713994c487eb8185
Nguyễn Đức Duyệt <duyetnguyen170104@gmail.com>
2026-03-22 06:32:51 +0700

    init project structure

---
+++ fe/js/data.js
@@ -0,0 +1,18 @@
+const artists = [
+    {
+        id: 1,
+        name: "Nguyễn Văn A",
+        role: "Ca sĩ",
+        price: 2000000,
+        image: "assets/images/singer1.jpg",
+        description: "Chuyên nhạc acoustic, ballad"
+    },
+    {
+        id: 2,
+        name: "Trần DJ B",
+        role: "DJ",
+        price: 3000000,
+        image: "assets/images/dj1.jpg",
+        description: "Chơi nhạc EDM, remix"
+    }
+];
